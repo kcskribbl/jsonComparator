3c4d567648af61f81b3661fa8023236b74da5f65
tokarthikc <32851916+tokarthikc@users.noreply.github.com>
2017-12-25 11:46:21 -0500

    Update CompareJson.java

--- CompareJson.java
+++ CompareJson.java
@@ -1,13 +1,20 @@
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.skyscreamer.jsonassert.Customization;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.comparator.CustomComparator;
 
+/**
+ * 
+ * @author gothai
+ *
+ */
 public class CompareJson {
-	
-	
+
 	/*
 	 * expectedResponseBody
 	 * 
@@ -17,8 +24,9 @@ public class CompareJson {
 	 * "CloseDoc()"} ] } }}
 	 */
 
+	//
 	public static String expectedResponseBody = "{\"menu\": {\r\n  \"id\": \"file\",\r\n  \"value\": \"File\", \r\n  \"date\" : \"2017-01-01\",\r\n  \"popup\": {\r\n    \"menuitem\": [\r\n      {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\r\n      {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\r\n      {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\r\n    ]\r\n  }\r\n}}";
-	public static String responseBody = "{\"menu\": {\r\n  \"id\": \"file\",\r\n  \"value\": \"File\", \r\n  \"date\" : \"2017-01-03\",\r\n  \"popup\": {\r\n    \"menuitem\": [\r\n      {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\r\n      {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\r\n      {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\r\n    ]\r\n  }\r\n}}";
+	public static String responseBody = "{\"menu\": {\r\n  \"id\": \"file\",\r\n  \"value\": \"File1\", \r\n  \"date\" : \"2017-01-03\",\r\n  \"popup\": {\r\n    \"menuitem\": [\r\n      {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\r\n      {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\r\n      {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\r\n    ]\r\n  }\r\n}}";
 
 	public static String responseBody_Error = "{\"menu\": {\r\n  \"id\": \"file\",\r\n  \"value\": \"File1\", \r\n  \"date\" : \"2017-01-03\",\r\n  \"popup\": {\r\n    \"menuitem\": [\r\n      {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\r\n      {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\r\n      {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\r\n    ]\r\n  }\r\n}}";
 
@@ -26,8 +34,27 @@ public class CompareJson {
 		happyCompare();
 		happyCompare_1();
 		errorCompare_1();
+		
+		List<String> jsonIdElementList = new ArrayList<String>();
+		jsonIdElementList.add("menu.date");
+		jsonIdElementList.add("menu.value");
+		happyCompareAsList(jsonIdElementList);
 	}
 
+	
+	private static void happyCompareAsList(List<String> jsonIdElementList) {
+		System.out.println("..Beginning happyCompareAsList...");
+		List<Customization> customizationList = new ArrayList();
+		for(String jsonIdElement : jsonIdElementList) {
+			customizationList.add(createCustomCustomization(jsonIdElement));
+		}
+		
+		JSONAssert.assertEquals(expectedResponseBody, responseBody, new CustomComparator(JSONCompareMode.LENIENT,
+				customizationList.toArray((new Customization[customizationList.size()]) )));
+		System.out.println("..Successfully compared happyCompareAsList...");
+	}
+	
+	
 	private static void happyCompare() {
 		System.out.println("..Beginning happyCompare...");
 		JSONAssert.assertEquals(expectedResponseBody, responseBody, new CustomComparator(JSONCompareMode.LENIENT,
@@ -56,5 +83,5 @@ public class CompareJson {
 	private static Customization createCustomCustomization(String jsonElementPath) {
 		return new Customization(jsonElementPath, (o1, o2) -> true);
 	}
-
+	
 }
